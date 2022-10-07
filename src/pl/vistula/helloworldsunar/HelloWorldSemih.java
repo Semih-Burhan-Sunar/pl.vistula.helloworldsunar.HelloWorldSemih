2f9802e0565fe96b15010c8731358ca074a419ce
Your Name <you@example.com>
2022-10-07 16:12:18 +0200

    zmien

--- src/pl/vistula/helloworldsunar/HelloWorldSemih.java
+++ src/pl/vistula/helloworldsunar/HelloWorldSemih.java
@@ -4,7 +4,7 @@ public class HelloWorldSemih {
     public static void main(String[] args) {
         System.out.println("HelloWorldVistulaSunar");
         System.out.println("HelloWorldVistulaSunaraaaaa");
-        System.out.println("HelloWorldVistulaSunaraaaaa");
+        System.out.println("HelloWorldVistulaSunarbvvgffgfaaaaa");
     }
 
 
