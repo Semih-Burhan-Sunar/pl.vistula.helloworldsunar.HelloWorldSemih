38bea5e13a73fb238b88ac17bea2ae0765cc7efc
Your Name <you@example.com>
2022-10-07 16:07:52 +0200

    Initial commit

---
+++ src/pl/vistula/helloworldsunar/HelloWorldSemih.java
@@ -0,0 +1,13 @@
+package pl.vistula.helloworldsunar;
+
+public class HelloWorldSemih {
+    public static void main(String[] args) {
+        println();System.out.println("HelloWorldVistulaSunar");
+    }
+
+    private static void println() {
+
+    }
+
+
+}
